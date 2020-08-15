d3262d95fcb69ad1b4cb79af4ab3f0cbcaf54a7c
Vishnu Rana <mvishnurana@gmail.com>
2020-08-15 18:45:48 +0800

    WIP: Temporarily commented form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Map from './Components/Map/Map';
+import BookingForm from './Components/BookingForm/BookingForm';
 
 function App() {
   return (
     <div>
       <Map />
+      {/* <BookingForm /> */}
     </div>
   );
 }
