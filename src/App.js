b521122027ce55a5df827d519e646ba585ed4cf8
Vishnu Rana <mvishnurana@gmail.com>
2020-10-20 22:15:17 +0800

    removed Form component from App; to only make Map the main component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Form from './Components/BookingForm/BookingForm'; 
 import Map from './Components/Map/Map';
 
 function App() {
   return <div>
-    <Form />
-    {/* <Map/> */}
+    <Map/>
   </div>
 }
 
