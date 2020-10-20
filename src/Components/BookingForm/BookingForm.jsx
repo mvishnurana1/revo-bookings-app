f697579b72667c4fda05c7fa64387a12fcfcd840
Vishnu Rana <mvishnurana@gmail.com>
2020-10-20 23:21:59 +0800

    Added Button and Form import in the same line for react-bootstrap

--- src/Components/BookingForm/BookingForm.jsx
+++ src/Components/BookingForm/BookingForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
+import { Button, Form } from 'react-bootstrap';
+
 import './BookingForm.css';
 
 class BookingForm extends Component {
