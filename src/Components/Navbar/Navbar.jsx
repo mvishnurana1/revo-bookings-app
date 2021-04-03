aea62393d2e6eeb4a3bf9d1ca9a1195bb25ba15e
Vishnu Rana <mvishnurana@gmail.com>
2021-04-03 19:28:33 +0800

    Improved Navbar to add app logo and make navbar like mark up like FaceBook

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,27 +4,50 @@ import FontAwesome from 'react-fontawesome';
 import './Navbar.css'; 
 
 const Navbar = () => {
-    return <div className="navbar">
+    return <div className="navbar" style={{ display: 'flex'}}> 
+    <button className="button">
         <FontAwesome
-            className="home logo"
-            name="home"
+            className="book logo"
+            name="book"
             size="3x"
         />
-        <FontAwesome
-            className="calendar logo"
-            name="calendar"
-            size="3x"
-        />
-        <FontAwesome
-            className="flag logo"
-            name="flag"
-            size="3x"
-        />
-        <FontAwesome
-            className="user-circle logo"
-            name="user-circle"
-            size="3x"
-        />
+    </button>
+
+    <div style={{ display: 'flex' }}>
+        <button className="button">
+            <FontAwesome
+                className="home logo"
+                name="home"
+                size="3x"
+            />
+        </button>
+
+        <button className="button">
+            <FontAwesome
+                className="calendar logo"
+                name="calendar"
+                size="3x"
+            />
+        </button>
+
+        <button className="button">
+            <FontAwesome
+                className="bell logo"
+                name="bell"
+                size="3x"
+            />
+        </button>
+    </div>
+
+    <button className="button">
+     <div className="accountLogo">
+            <FontAwesome
+                className="user-circle logo"
+                name="user-circle"
+               size="3x"
+            />
+     </div>
+    </button>
     </div>
 }
 
